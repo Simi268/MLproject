80d6c87210b907ed7b0a63e1f21746c03294471a
Simi268 <simikumari571@gmail.com>
2025-12-22 16:06:10 +0530

    Dockerized ML app with separate training and inference requirements

--- app.py
+++ app.py
@@ -30,14 +30,17 @@ def predict_datapoint():
 
     pred_df = data.get_data_as_dataframe()
 
+
     predict_pipeline = PredictPipeline()
     results = predict_pipeline.predict(pred_df)
+
 
     return render_template('home.html', results=results[0])
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
+
 
  
